15623d5f41320d43c4af339a5ef94ebee460c063
salehi <akbarsalehi@gmail.com>
2020-10-06 19:47:44 +0330

    session 3

--- app/src/main/java/ir/laitec/laitecandroidmehr99/MainActivity.java
+++ app/src/main/java/ir/laitec/laitecandroidmehr99/MainActivity.java
@@ -11,7 +11,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MyTag","in On Create!!!");
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MyTag","in On Start!!!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MyTag","in On Resume!!!");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MyTag","in On Pause!!!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MyTag","in On Stop!!!");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MyTag","in On ReStart!!!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MyTag","in On Destroy!!!");
+    }
 }
